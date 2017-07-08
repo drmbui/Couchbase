a963398816f4a82be372861743bb1a25c72574e6
Minh Bui <dr@mbui.me>
2017-07-07 20:07:46 -0500

    Add files via upload

--- NET/CouchbasePortTester/CouchbasePortTester/Write/Doc.cs
+++ NET/CouchbasePortTester/CouchbasePortTester/Write/Doc.cs
@@ -5,13 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Couchbase;
 using Couchbase.Configuration.Client;
-
+using System.Threading;
 
 namespace CouchbasePortTester.Write
 {
     public class Doc
     {
         private static readonly Cluster cluster = new Cluster();
+        /// <summary>
+        /// 
+        /// </summary>
         public void Create()
         {
 
@@ -19,44 +22,70 @@ namespace CouchbasePortTester.Write
             {
                 Servers = new List<Uri>
                 {
-                    new Uri("http://vcsmc2rqaalnd04.ual.com:8091"),
-                    new Uri("http://vcsmc2rqaalnd05.ual.com:8091"),
-                    new Uri("http://vcsmc2rqaalnd06.ual.com:8091"),
-                    new Uri("http://vcsmc2rqaalnd07.ual.com:8091")
+                    new Uri("http://vcsmc2rqbfpnd01.ual.com:8091"),
+                    new Uri("http://vcsmc2rqbfpnd02.ual.com:8091"),
+                    new Uri("http://vcsmc2rqbfpnd03.ual.com:8091"),
+                    new Uri("http://vcsmc2rqbfpnd04.ual.com:8091"),
+                    new Uri("http://vcsmc2rqbfpnd05.ual.com:8091"),
+                    new Uri("http://vcsmc2rqbfpnd06.ual.com:8091"),
+                    new Uri("http://vcsmc2rqbfpnd07.ual.com:8091"),
+                    new Uri("http://vcsmc2rqbfpnd08.ual.com:8091")
                 }
             };
 
             var cluster = new Cluster(config);
-            var bucket = cluster.OpenBucket("minhTest", "123456");
+            var bucket = cluster.OpenBucket("minhTest");
 
             //using (var bucket = cluster.OpenBucket("minhTest", "123456"))
             // {
-            var document = new Document<dynamic>
+            for (int i = 0; i < 100; i++)
             {
-
-                Id = "testKey",
-                Content = new
+                var document = new Document<dynamic>
                 {
-                    name = "Minh Bui test insert document"
+
+                    Id = "testKey" + Guid.NewGuid(),
+                    Content = new
+                    {
+                        name = "Minh Bui test insert document"
+                    }
+                };
+
+                try
+                {
+                    var upsert = bucket.Upsert(document);
+
+                    if (upsert.Success)
+                    {
+                        Thread.Sleep(50);
+                      
+                            var get = bucket.GetDocument<dynamic>(document.Id);
+                            document = get.Document;
+                            var msg = string.Format("{0} {1}!", document.Id, document.Content.name);
+                            msg = i.ToString() + " " + msg;
+                            //Console.WriteLine(msg);
+
+                    }
+                    else
+                    {
+                        string ms = upsert.Message;
+                        ms = i.ToString() + " message failed!";
+                        Console.WriteLine(ms);
+                        
+                    }
+
                 }
-            };
+                catch (Exception e)
+                {
+                    string m = e.ToString();
+                }
 
+            }
 
-            var upsert = bucket.Insert(document);
-            if (upsert.Success)
-            {
-                var get = bucket.GetDocument<dynamic>(document.Id);
-                document = get.Document;
-                var msg = string.Format("{0} {1}!", document.Id, document.Content.name);
-                Console.WriteLine(msg);
-
-            };
+            Console.ReadLine();
 
             cluster.Dispose();
             bucket.Dispose();
 
         }
-
-  
     }
 }
